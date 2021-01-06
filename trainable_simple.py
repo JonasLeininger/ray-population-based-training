fa323fb754654d75bab05614dbee958d691cc40f
JonasLeininger <jonas.leininger@rewe-digital.com>
2021-01-06 14:38:26 +0100

    implement reporter and objective from PBT paper

--- trainable_simple.py
+++ trainable_simple.py
@@ -1,9 +1,7 @@
+import numpy as np
 from ray import tune
 
 from annotations import override
-
-def objective(x, a, b):
-    return a * (x**0.5) + b
 
 
 class SimpleTrainer(tune.Trainable):
@@ -11,12 +9,17 @@ class SimpleTrainer(tune.Trainable):
 
     @override(tune.Trainable)
     def setup(self, config):
-        self.x = 0
-        self.a = config["a"]
-        self.b = config["b"]
+        self.theta = config["theta"]
+        self.h = config["h"]
+        self.alpha = config["alpha"]
+        self.objective = lambda theta: 1.2 - np.sum(theta**2)
+        self.surrogateObjective = lambda theta, h: 1.2 - np.sum(h * theta**2)
     
     @override(tune.Trainable)
     def step(self):
-        score = objective(self.x, self.a, self.b)
-        self.x += 1
-        return {"score": score}
+        div_surrogateObjective = -2.0 * self.h * self.theta
+        div_loss = -div_surrogateObjective # gradient ascent
+
+        self.theta -= div_loss * self.alpha
+        score = self.objective(self.theta)
+        return {"score": score, "h": self.h, "theta": self.theta}
--- tune_pbt_simple_model.py
+++ tune_pbt_simple_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ray
 import ray.tune as tune
 from ray.tune import CLIReporter
@@ -7,14 +8,19 @@ from trainable_simple import SimpleTrainer
 def main():
 
     ray.init(address="auto")
+    reporter = CLIReporter()
+    reporter.add_metric_column("h")
+    reporter.add_metric_column("theta")
 
     analysis = tune.run(
         SimpleTrainer,
-        stop={"training_iteration": 20},
+        stop={"training_iteration": 180},
         config={
-            "a": 2,
-            "b": 4
-        }
+            "theta": np.array([0.9, 0.9]),
+            "h": np.array([1.,0.]),
+            "alpha": 0.01
+        },
+        progress_reporter=reporter
     )
     print('best config: ', analysis.get_best_config(metric="score", mode="max"))
 
